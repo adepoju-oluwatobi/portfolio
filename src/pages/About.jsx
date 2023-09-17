5bd777e29f50946a75de1746c6005606e681346b
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-09-17 12:08:52 +0100

    moved the bio

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,10 @@ import Github from "../assets/github.svg";
 import Email from "../assets/mail.svg";
 import profilePics from "../assets/profile-pics.png";
 import { Link } from "react-router-dom";
+import Cert1 from '../assets/cert1.jpeg'
+import Cert2 from '../assets/cert2.jpeg'
+import Cert3 from '../assets/cert3.png'
+import Cert4 from '../assets/cert4.png'
 
 function About() {
   const [isNavMenuOpen, setIsNavMenuOpen] = useState(false);
@@ -40,7 +44,8 @@ function About() {
           {/** Bio */}
           <div className="about text-white">
             <div className="p-2 h-[400px] overflow-y-auto">
-              <p>About Me</p>
+              <div>
+              <p className="font-bold text-2xl border-b-2 w-fit mb-2">About Me</p>
               <p>
                 The passion for developing website and user friendly interface
                 led me to the study of front end development in which i am
@@ -52,8 +57,39 @@ function About() {
                 is one of them. Also, I am an enthusiastic teacher who loves to
                 impact knowledge. With a teaching experience of 8 years plus, I
                 can say i have impacted more than i could have ever wished for.
-                Here is my Git hub profile link: https://github.com/Hue-Ray
               </p>
+              </div>
+
+              <div>
+              <p className="font-bold text-2xl border-b-2 w-fit mb-2">Certification</p>
+              <div className="flex flex-col gap-4">
+              <div>
+              <img src={Cert1} alt="" />
+              <p>
+                This is a Certification responsive website development by FreeCodeCamp, one of the most recorgnised online code learning platform. Here is the link to the information about the certificate
+              </p>
+              <div className="mt-2 p-2 bg-black rounded w-fit">
+              <a href="https://www.freecodecamp.org/certification/tobi_frontend/responsive-web-design" target="_blank"
+                >Check Certificate</a>
+              </div>
+              </div>
+              <img src={Cert3} alt="" />
+              <img src={Cert4} alt="" />
+              <img src={Cert2} alt="" />
+              </div>
+              <p>
+                The passion for developing website and user friendly interface
+                led me to the study of front end development in which i am
+                certified and proficient in HTML, CSS, JavaScript, React Js,
+                Next Js, Vite, Tailwind Css, Sass and Boostrap using these
+                skills to create highly responsive and user friendly websites. I
+                have under gone a 4 years course in the field of computer
+                science and also took some online courses in which FreeCodeCamp
+                is one of them. Also, I am an enthusiastic teacher who loves to
+                impact knowledge. With a teaching experience of 8 years plus, I
+                can say i have impacted more than i could have ever wished for.
+              </p>
+              </div>
               <div className="flex gap-10 mt-2">
             <a
               href="https://www.linkedin.com/in/oluwatobi-adepoju-4a242898/"
