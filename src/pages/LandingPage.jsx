b7fc22fa94364d28aeff073bf4092b94135eefbc
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-09-17 08:41:48 +0100

    moved the bio

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,60 +1,79 @@
-import React from 'react'
-import Open from '../assets/chevrons-right.svg'
-import Linkedin from '../assets/linkedin.svg'
+import React, { useState } from "react";
+import Open from "../assets/chevrons-right.svg";
+import Linkedin from "../assets/linkedin.svg";
+import Github from "../assets/github.svg";
+import Email from "../assets/mail.svg";
 
 function LandingPage() {
+  const [isNavMenuOpen, setIsNavMenuOpen] = useState(false);
+
+  const toggleNavMenu = () => {
+    setIsNavMenuOpen(!isNavMenuOpen);
+  };
+
   return (
-    <div className='h-[100vh] w-[100%] bg-[url(./assets/BACKGROUND.png)]'>
+    <div className="h-[100vh] w-[100%] bg-[url(./assets/BACKGROUND.png)]">
+      {/** Menu */}
+      <div className="pt-4 pl-4">
+        <div
+          className="bg-[#5F439B] w-fit flex items-center gap-2 p-2 rounded cursor-pointer"
+          onClick={toggleNavMenu}
+        >
+          <div className="bg-black w-fit p-1 rounded-full">
+            <img src={Open} alt="" />
+          </div>
+          <p className="text-white">Menu</p>
+        </div>
+      </div>
+      {/** End of Menu code */}
 
-        {/** Menu */}
-        <div className='pt-4 pl-4'>
-        <div className='bg-[#5F439B] w-fit flex items-center gap-2 p-2 rounded '>
-            <div className='bg-black w-fit p-1 rounded-full'>
-                <img src={Open} alt="" />
+      <div className="flex items-center gap-4">
+        {/** Menu side bar */}
+        <div className="pt-4">
+          <div className='side-bar w-2 h-[80vh] bg-[#5F439B] rounded'></div>
+        </div>
+        {/** End */}
+
+        <div>
+          {/** Bio */}
+          <div className="text-white mt-[50%]">
+            <div className="p-2">
+              <p>Hello, I am Adepoju Oluwatobi</p>
+              <p className="font-bold text-[34px]">WEB DEVELOPER</p>
+              <p className="text-[24px] font-bold">{`< FULL STACK />`}</p>
             </div>
-            <p>Menu</p>
-        </div>
-        </div>
-        {/**End of Menu code */}
+          </div>
 
-        
-        <div className='flex items-center gap-4'>
-
-            {/**Menu side bar */}
-        <div className='pt-4'>
-            <div className='w-2 h-[80vh] bg-[#5F439B] rounded'>
-            </div>
-        </div>
-            {/**End */}
-
-         <div>
-               {/**Bio */}
-        <div className='text-white mt-[50%]'>
-            <div className='p-2'>
-                <p>Hello, i am Adepoju Oluwatobi</p>
-                <p className='font-bold text-[34px]'>WEB DEVELOPER</p>
-                <p className='text-[24px] font-bold'>{`< FULL STACK />`}</p>
-            </div>
-        </div>
-
-        <div className='flex gap-10'>
-            <div className='bg-[#5F439B] w-fit p-4 rounded'>
+          <div className="flex gap-10">
+            <a
+              href="https://www.linkedin.com/in/oluwatobi-adepoju-4a242898/"
+              target="_blank"
+            >
+              <div className="bg-[#5F439B] w-fit p-4 rounded">
                 <img src={Linkedin} alt="" />
-            </div>
+              </div>
+            </a>
 
-            <div className='bg-[#5F439B] w-fit p-4 rounded'>
-                <img src={Linkedin} alt="" />
-            </div>
+            <a href="https://github.com/adepoju-oluwatobi" target="_blank">
+              <div className="bg-[#5F439B] w-fit p-4 rounded">
+                <img src={Github} alt="" />
+              </div>
+            </a>
 
-            <div className='bg-[#5F439B] w-fit p-4 rounded'>
-                <img src={Linkedin} alt="" />
-            </div>
+            <a href="mailto:kunletobi4@gmail.com" target="_blank">
+              <div className="bg-[#5F439B] w-fit p-4 rounded">
+                <img src={Email} alt="" />
+              </div>
+            </a>
+          </div>
         </div>
+      </div>
 
-         </div>
-        </div>
+      <div className={`nav-menu relative top-[-80%] ${isNavMenuOpen ? 'block' : 'hidden'}`}>
+        <div className="w-[90%] h-[80vh] bg-white border-r-[8px] border-[#5F439B]"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
